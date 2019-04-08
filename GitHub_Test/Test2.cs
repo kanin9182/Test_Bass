c02ec703fa496a71410d419c86078793ead1304c
kanin9182 <kanin9182@hotmail.com>
2019-04-08 22:41:13 +0700

    It has add file Test2.cs

---
+++ GitHub_Test/Test2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Test
+{
+    class Test2
+    {
+        public class Test
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Test(string Name, int Age)
+            {
+                this.Name = Name;
+                this.Age = Age;
+            }
+        }
+    }
+}
